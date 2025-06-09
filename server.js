ed2771b61f08e5d7a3ef12d01a4f28774362eeaa
unknown <jiggsgadhia@gmail.com>
2025-06-09 15:06:19 +0530

    code update

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
 app.use("/api/auth", authRoutes);
 app.use("/api/doctors", doctorRoutes);
 
-mongoose.connect(process.env.MONGO_URI).catch((err) => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error(err));
